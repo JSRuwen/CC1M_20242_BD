2800f44b4c15835571f593ecbb6826cda94ec39d
Rhuan Santos <124512081+JSRuwen@users.noreply.github.com>
2024-11-02 14:11:30 -0300

    Add files via upload

--- DDL/estudoCaso_1.sql
+++ DDL/estudoCaso_1.sql
@@ -1,93 +1,153 @@
-#drop schema estudoCaso_1
+#drop schema estudoCaso_1;
 CREATE SCHEMA estudoCaso_1;
 
 USE estudoCaso_1;
 
 #criando tabelas simples
 create table if not exists fornecedores(
-FORNEC_COD int primary key,
-FORNEC_NOME varchar(45) not null,
-FORNEC_RUA varchar(45) not null,
-FORNEC_NUMRUA int,
-FORNEC_BAIRRO varchar(45) not null,
-FORNEC_CIDADE varchar(45) not null,
-FORNEC_ESTADO varchar(45) not null,
-FORNEC_PAIS varchar(30) not null,
-FORNEC_CODPOSTAL varchar(10) not null,
-FORNEC_TELEFONE varchar(15) not null,
-FORNEC_CONTATO text not null
+cod_forn int primary key,
+nome_forn varchar(45) not null,
+rua_forn varchar(45) not null,
+numrua_forn int,
+bairro_forn varchar(45) not null,
+cidade_forn varchar(45) not null,
+estado_forn varchar(45) not null,
+pais_forn varchar(30) not null,
+codpostal_forn varchar(10) not null,
+telefone_forn varchar(15) not null,
+contato_forn text not null
 );
+
+alter table fornecedores add registro text; 
+alter table fornecedores change column registro historicoContato text;
+alter table fornecedores modify historicoContato date;
+alter table fornecedores drop column historicoContato;
+
 
 create table if not exists filiais(
-FILIAL_COD int primary key,
-FILIAL_NOME varchar(45) not null,
-FILIAL_RUA varchar(100) not null,
-FILIAL_NUMRUA int,
-FILIAL_BAIRRO varchar(50) not null,
-FILIAL_CIDADE varchar(50) not null,
-FILIAL_ESTADO varchar(50) not null,
-FILIAL_PAIS varchar(50) not null,
-FILIAL_CODPOSTAL varchar(10) not null,
-FILIAL_CAPACIDADE text not null
+cod_filial int primary key,
+nome_filial varchar(45) not null,
+rua_filial varchar(100) not null,
+numrua_filial int,
+bairro_filial varchar(50) not null,
+cidade_filial varchar(50) not null,
+estado_filial varchar(50) not null,
+pais_filial varchar(50) not null,
+codpostal_filial varchar(10) not null,
+capacidade_filial text not null
 );
+
+alter table filiais add registro text; 
+alter table filiais change column registro historicoContato text;
+alter table filiais modify historicoContato date;
+alter table filiais drop column historicoContato;
+
 
 create table if not exists produtos(
-PROD_COD int primary key,
-PROD_NOME varchar(50) not null,
-PROD_DESCRICAO text not null,
-PROD_ESPECTEC text not null,
-PROD_QUANT decimal(10,3) not null,
-PROD_PRECOUNIT decimal(10,2) not null,
-PROD_UNIDMEDIDA varchar(10) not null,
-PROD_ESTOQ_MIN decimal(10,3) not null
+cod_prod int primary key,
+nome_prod varchar(50) not null,
+descricao_prod text not null,
+espectec_prod text not null,
+quant_prod decimal(10,3) not null,
+precounid_prod decimal(10,2) not null,
+unidmedida_prod varchar(10) not null,
+estoqMin_prod decimal(10,3) not null
 );
+
+alter table produtos add registro text; 
+alter table produtos change column registro histFabricacao text;
+alter table produtos modify histFabricacao time;
+alter table produtos drop column histFabricacao;
+
 
 #criando tabelas com dependencias de foreign key
-
-#CRIANDO A TABELA pedidos
 create table if not exists pedidos(
-PED_CODIGO int primary key,
-PED_DATA date not null,
-PED_HORA time not null,
-PED_PREVISAO date not null,
-PED_STATUS ENUM('pendente','concluído','a caminho'),
-PED_FORNECEDOR int not null,
-constraint FK_FORNECEDOR foreign key (PED_FORNECEDOR) references fornecedores(FORNEC_COD)
+cod_pedi int primary key,
+data_pedi date not null,
+hora_pedi time not null,
+previsao_pedi date not null,
+status_pedi ENUM('pendente','concluído','a caminho'),
+fornec_pedi int not null,
+constraint FK_fornecedor foreign key (fornec_pedi) references fornecedores(cod_forn)
 );
 
+alter table pedidos add registro text; 
+alter table pedidos change column registro histFabricacao text;
+alter table pedidos modify histFabricacao time;
+alter table pedidos drop column histFabricacao;
+
+
 create table if not exists recebimentos(
-RECEB_DATA date not null,
-RECEB_HORA time not null,
-RECEB_QUANT_PROD decimal(10,3),
-RECEB_CONDICAO text not null,
-RECEB_PEDIDOS int primary key,
-constraint FK_PEDIDOS foreign key(RECEB_PEDIDOS) references pedidos(PED_CODIGO));
+data_receb date not null,
+hora_receb time not null,
+quantprod_receb decimal(10,3),
+condicao_receb text not null,
+pedidos_receb int primary key,
+constraint FK_pedidos foreign key(pedidos_receb) references pedidos(cod_pedi));
+
+alter table recebimentos add comentario text; 
+alter table recebimentos change column comentario descricao text;
+alter table recebimentos modify descricao varchar(500);
+alter table recebimentos drop column descricao;
+
 
 #criando tabelas associativas
 
-create table if not exists PEDIDOS_PRODUTOS (
-PDPR_PEDIDOS int,
-PDPR_PRODUTOS int,
-primary key(PDPR_PEDIDOS,PDPR_PRODUTOS),
-PDPR_QUANT decimal(10,3) not null,
-constraint PDPR_FK_PEDIDOS foreign key(PDPR_PEDIDOS) references pedidos(PED_CODIGO),
-constraint PDPR_FK_PRODUTOS foreign key(PDPR_PRODUTOS) references produtos(PROD_COD)
+create table if not exists pedidos_produtos (
+pdpr_pedidos int,
+pdpr_produtos int,
+pdpr_quant decimal(10,3) not null,
+primary key(pdpr_pedidos,pdpr_produtos),
+constraint pdpr_FK_pedidos foreign key(pdpr_pedidos) references pedidos(cod_pedi),
+constraint pdpr_FK_produtos foreign key(pdpr_produtos) references produtos(cod_prod)
 );
 
+alter table pedidos_produtos add registro text; 
+alter table pedidos_produtos change column registro histFabricacao text;
+alter table pedidos_produtos modify histFabricacao time;
+alter table pedidos_produtos drop column histFabricacao;
 
-create table if not exists FILIAIS_PRODUTOS (
-FLPR_FILIAL int,
-FLPR_PRODUTOS int,
-primary key(FLPR_FILIAL,FLPR_PRODUTOS),
-FLPR_QUANT decimal(10,3) not null,
-constraint FLPR_FK_FILIAIS foreign key(FLPR_FILIAL) references filiais(FILIAL_COD),
-constraint FLPR_FK_PRODUTOS foreign key(FLPR_PRODUTOS) references produtos(PROD_COD)
+
+create table if not exists filiais_produtos (
+flpr_filial int,
+flpr_produto int,
+flpr_quant decimal(10,3) not null,
+primary key(flpr_filial,flpr_produto),
+constraint flpr_FK_filial foreign key(flpr_filial) references filiais(cod_filial),
+constraint flpr_FK_produto foreign key(flpr_produto) references produtos(cod_prod)
 );
 
-create table if not exists FORNECEDORES_PRODUTOS (
-FRPR_FORNECEDOR int,
-FRPR_PRODUTOS int,
-primary key(FRPR_FORNECEDOR,FRPR_PRODUTOS),
-constraint FRPR_FK_FORNECEDORES foreign key(FRPR_FORNECEDOR) references fornecedores(FORNEC_COD),
-constraint FRPR_FK_PRODUTOS foreign key(FRPR_PRODUTOS) references produtos(PROD_COD)
+alter table filiais_produtos add estoqueMin_fipr int;
+alter table filiais_produtos change column estoqueMin_fipr fipr_capacidadeEstoque int;
+alter table filiais_produtos modify column fipr_capacidadeEstoque int not null;
+alter table filiais_produtos drop column fipr_capacidadeEstoque;
+
+
+create table if not exists fornec_produtos (
+frpr_fornec int,
+frpr_produto int,
+primary key(frpr_fornec,frpr_produto),
+constraint frpr_FK_fornec foreign key(frpr_fornec) references fornecedores(cod_forn),
+constraint frpr_FK_produto foreign key(frpr_produto) references produtos(cod_prod)
 );
+
+alter table fornec_produtos ADD avaliacao text not null;
+alter table fornec_produtos CHANGE COLUMN avaliacao fopr_avaliacao text;
+alter table fornec_produtos MODIFY COLUMN fopr_avaliacao decimal(2,0);
+alter table fornec_produtos DROP COLUMN fopr_avaliacao;
+
+#># criando uma tabela adicional (drop) #<#
+
+create table if not exists empresas(
+cod_empresas int primary key,
+nome_empresas varchar(45) not null,
+rua_empresas varchar(100) not null,
+numrua_empresas int,
+bairro_empresas varchar(50) not null,
+cidade_empresas varchar(50) not null,
+estado_empresas varchar(50) not null,
+pais_empresas varchar(50) not null,
+codpostal_empresas varchar(10) not null
+);
+
+drop table empresas
